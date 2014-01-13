afd7d8111d0a9052eea14ce0a740d4a64cbe200b
Julien Grimault <grimaultjulien@gmail.com>
2014-01-13 23:54:22 +0800

    Add Rake task to download latest 500px photos and store them in redid.

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ ruby '2.0.0'
 # Web server
 gem 'unicorn'
 
+# Storage
+gem 'redis'
 
 gem 'sinatra'
 gem 'haml'
@@ -19,6 +21,10 @@ gem 'sprockets-sass'
 gem 'bourbon'
 
 gem 'newrelic_rpm'
+
+#Utilities
+gem 'rake'
+gem 'httparty'
 
 group :development do
   # Reloading in development
---
+++ utils/five_hundred_px.rb
@@ -0,0 +1,16 @@
+require 'httparty'
+
+module Utils
+  class FiveHundredPx
+    include HTTParty
+    base_uri 'https://api.500px.com/v1'
+
+    def initialize(consumer_key)
+      self.class.default_params :consumer_key => consumer_key
+    end
+
+    def user_photos(username, options={})
+      self.class.get("/photos?feature=user&username=#{username}", options)
+    end
+  end
+end
